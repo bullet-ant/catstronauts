2ddcf663bbf71d298bc5f872e90ff75af65eb222
bullet-ant <aman.kr.2129@gmail.com>
2024-12-09 09:15:17 +0530

    add track fetching functionality with new resolvers and schema updates

--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -1,13 +1,21 @@
 const resolvers = {
   Query: {
+    // get tracks array for homepage grid
     tracksForHome: (_, __, { dataSources }) => {
       return dataSources.trackAPI.getTracksForHome();
+    },
+    // get a single track by ID, for the Track page
+    track: (_, { id }, { dataSources }) => {
+      return dataSources.trackAPI.getTrack(id);
     },
   },
 
   Track: {
     author: ({ authorId }, _, { dataSources }) => {
       return dataSources.trackAPI.getAuthor(authorId);
+    },
+    modules: ({ id }, _, { dataSources }) => {
+      return dataSources.trackAPI.getTrackModules(id);
     },
   },
 };
