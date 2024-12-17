929816857d799aa09b6c4ed0549e7dc8479fa705
bullet-ant <aman.kr.2129@gmail.com>
2024-12-17 22:15:17 +0530

    add module fetching functionality with new resolver and schema updates

--- server/src/schema.js
+++ server/src/schema.js
@@ -7,6 +7,8 @@ const typeDefs = gql`
     tracksForHome: [Track!]!
     "Fetch a specific track, provided a track's ID"
     track(id: ID!): Track
+    "Fetch a specific module, provided a module's ID"
+    module(id: ID!): Module
   }
 
   "A track is a group of Modules that teaches about a specific topic"
@@ -38,6 +40,10 @@ const typeDefs = gql`
     title: String!
     "The Module's length in minutes"
     length: Int
+    "The module's text-based description, can be in markdown format. In case of a video, it will be the enriched transcript"
+    videoUrl: String
+    "The Module's content in text form"
+    content: String
   }
 
   "Author of a complete Track or a Module"
